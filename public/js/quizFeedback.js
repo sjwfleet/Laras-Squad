e25663d6c07923ade8e6667e7adb7526fd96ef96
Samuel Fleet <sfleet@ucsd.edu>
2021-03-02 16:28:40 -0800

    Changes

--- public/js/quizFeedback.js
+++ public/js/quizFeedback.js
@@ -1,9 +1,9 @@
 // learned from https://simplestepscode.com/javascript-quiz-tutorial/
 'use strict';
-
+let score = 0;
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+    initializePage();
 })
 
 /*
@@ -11,20 +11,22 @@ $(document).ready(function() {
  */
 function initializePage() {
 
-	console.log("Javascript connected!");
+    console.log("Javascript connected!");
 
     $(".q").click(answerSelected);
+    $(".q").removeClass("quiz-button");
+    $(".q").addClass("correct");
     console.log("wtf");
-    
+
 }
-  
+
 
 function answerSelected(e) {
     e.preventDefault();
     console.log("getting there");
 
-	var x = $(this).text();
-	$(this).text(answer(x));
+    var x = $(this).text();
+    $(this).text(answer(x));
 }
 
 function answer(selected) {
@@ -34,25 +36,33 @@ function answer(selected) {
     $.getJSON('/quizData', function(data) {
         data = data['modInfo'];
         console.log(data);
+
         for (let i = 0; i < data.length; i++) {
             let d = data[i];
+
             if (d.id == qid) {
                 // do your quiz checking stuff here
                 console.log(d);
-                
-                
-                break;
+
+                console.log(d.id);
+
+                for (let i=0; i < d['quiz'].length; i++) {
+
+                    let did = d['quiz'][i]['answer'];
+                    console.log(did + " THIS IS THE ANSWER");
+                    if (did == selected) {
+
+                        score++;
+                        console.log("done did it");
+
+                    }
+                    else {
+
+                        console.log("wrong!!");
+                    }
+                 }
             }
         }
     });
-    var data = require('../data/modInfo.json')['quiz'];
-    var question = data['question'];
-    if (selected == question.answer) {
-        return "Correct!"
-    }
-    
-    else {
-        return "Incorrect."
-    }
 }
 
